cb8e8309708c179e8161426432d7acad09e01108
Cristian Ionita <Cristi34@users.noreply.github.com>
2019-01-17 16:39:13 +0200

    started Code Project Design Patterns, windows forms UI

---
+++ testing2/testing2/WindowsCustomerUI/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace WindowsCustomerUI
+{
+    partial class fCustomerScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCustomerType = new System.Windows.Forms.Label();
+            this.lblCustomerName = new System.Windows.Forms.Label();
+            this.lblPhoneNumber = new System.Windows.Forms.Label();
+            this.lblBillAmount = new System.Windows.Forms.Label();
+            this.lblBillDate = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.cbCustomerType = new System.Windows.Forms.ComboBox();
+            this.tbCustomerName = new System.Windows.Forms.TextBox();
+            this.tbPhoneNumber = new System.Windows.Forms.TextBox();
+            this.tbBillDate = new System.Windows.Forms.TextBox();
+            this.tbBillAmount = new System.Windows.Forms.TextBox();
+            this.rtbAddress = new System.Windows.Forms.RichTextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblCustomerType
+            // 
+            this.lblCustomerType.AutoSize = true;
+            this.lblCustomerType.Location = new System.Drawing.Point(12, 9);
+            this.lblCustomerType.Name = "lblCustomerType";
+            this.lblCustomerType.Size = new System.Drawing.Size(104, 17);
+            this.lblCustomerType.TabIndex = 0;
+            this.lblCustomerType.Text = "Customer Type";
+            // 
+            // lblCustomerName
+            // 
+            this.lblCustomerName.AutoSize = true;
+            this.lblCustomerName.Location = new System.Drawing.Point(12, 43);
+            this.lblCustomerName.Name = "lblCustomerName";
+            this.lblCustomerName.Size = new System.Drawing.Size(109, 17);
+            this.lblCustomerName.TabIndex = 1;
+            this.lblCustomerName.Text = "Customer Name";
+            // 
+            // lblPhoneNumber
+            // 
+            this.lblPhoneNumber.AutoSize = true;
+            this.lblPhoneNumber.Location = new System.Drawing.Point(12, 77);
+            this.lblPhoneNumber.Name = "lblPhoneNumber";
+            this.lblPhoneNumber.Size = new System.Drawing.Size(101, 17);
+            this.lblPhoneNumber.TabIndex = 2;
+            this.lblPhoneNumber.Text = "Phone number";
+            // 
+            // lblBillAmount
+            // 
+            this.lblBillAmount.AutoSize = true;
+            this.lblBillAmount.Location = new System.Drawing.Point(300, 9);
+            this.lblBillAmount.Name = "lblBillAmount";
+            this.lblBillAmount.Size = new System.Drawing.Size(78, 17);
+            this.lblBillAmount.TabIndex = 3;
+            this.lblBillAmount.Text = "Bill Amount";
+            // 
+            // lblBillDate
+            // 
+            this.lblBillDate.AutoSize = true;
+            this.lblBillDate.Location = new System.Drawing.Point(300, 43);
+            this.lblBillDate.Name = "lblBillDate";
+            this.lblBillDate.Size = new System.Drawing.Size(60, 17);
+            this.lblBillDate.TabIndex = 4;
+            this.lblBillDate.Text = "Bill Date";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(300, 77);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(60, 17);
+            this.lblAddress.TabIndex = 5;
+            this.lblAddress.Text = "Address";
+            // 
+            // cbCustomerType
+            // 
+            this.cbCustomerType.FormattingEnabled = true;
+            this.cbCustomerType.Location = new System.Drawing.Point(145, 6);
+            this.cbCustomerType.Name = "cbCustomerType";
+            this.cbCustomerType.Size = new System.Drawing.Size(121, 24);
+            this.cbCustomerType.TabIndex = 6;
+            // 
+            // tbCustomerName
+            // 
+            this.tbCustomerName.Location = new System.Drawing.Point(145, 40);
+            this.tbCustomerName.Name = "tbCustomerName";
+            this.tbCustomerName.Size = new System.Drawing.Size(149, 22);
+            this.tbCustomerName.TabIndex = 7;
+            // 
+            // tbPhoneNumber
+            // 
+            this.tbPhoneNumber.Location = new System.Drawing.Point(145, 74);
+            this.tbPhoneNumber.Name = "tbPhoneNumber";
+            this.tbPhoneNumber.Size = new System.Drawing.Size(149, 22);
+            this.tbPhoneNumber.TabIndex = 8;
+            // 
+            // tbBillDate
+            // 
+            this.tbBillDate.Location = new System.Drawing.Point(384, 38);
+            this.tbBillDate.Name = "tbBillDate";
+            this.tbBillDate.Size = new System.Drawing.Size(149, 22);
+            this.tbBillDate.TabIndex = 10;
+            // 
+            // tbBillAmount
+            // 
+            this.tbBillAmount.Location = new System.Drawing.Point(384, 6);
+            this.tbBillAmount.Name = "tbBillAmount";
+            this.tbBillAmount.Size = new System.Drawing.Size(149, 22);
+            this.tbBillAmount.TabIndex = 11;
+            // 
+            // rtbAddress
+            // 
+            this.rtbAddress.Location = new System.Drawing.Point(384, 77);
+            this.rtbAddress.Name = "rtbAddress";
+            this.rtbAddress.Size = new System.Drawing.Size(228, 89);
+            this.rtbAddress.TabIndex = 12;
+            this.rtbAddress.Text = "";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(15, 121);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 34);
+            this.btnAdd.TabIndex = 13;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(96, 121);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 34);
+            this.btnUpdate.TabIndex = 14;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(177, 121);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 34);
+            this.btnDelete.TabIndex = 15;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(258, 121);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 34);
+            this.btnCancel.TabIndex = 16;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // fCustomerScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.rtbAddress);
+            this.Controls.Add(this.tbBillAmount);
+            this.Controls.Add(this.tbBillDate);
+            this.Controls.Add(this.tbPhoneNumber);
+            this.Controls.Add(this.tbCustomerName);
+            this.Controls.Add(this.cbCustomerType);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.lblBillDate);
+            this.Controls.Add(this.lblBillAmount);
+            this.Controls.Add(this.lblPhoneNumber);
+            this.Controls.Add(this.lblCustomerName);
+            this.Controls.Add(this.lblCustomerType);
+            this.Name = "fCustomerScreen";
+            this.Text = "Customer Screen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCustomerType;
+        private System.Windows.Forms.Label lblCustomerName;
+        private System.Windows.Forms.Label lblPhoneNumber;
+        private System.Windows.Forms.Label lblBillAmount;
+        private System.Windows.Forms.Label lblBillDate;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.ComboBox cbCustomerType;
+        private System.Windows.Forms.TextBox tbCustomerName;
+        private System.Windows.Forms.TextBox tbPhoneNumber;
+        private System.Windows.Forms.TextBox tbBillDate;
+        private System.Windows.Forms.TextBox tbBillAmount;
+        private System.Windows.Forms.RichTextBox rtbAddress;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
