ad0b1a4542979319118f1cedfa401705df0e5bf8
Cristian Ionita <Cristi34@users.noreply.github.com>
2019-01-17 17:56:13 +0200

    cp design patterns further dev

--- testing2/testing2/WindowsCustomerUI/Form1.Designer.cs
+++ testing2/testing2/WindowsCustomerUI/Form1.Designer.cs
@@ -152,6 +152,7 @@
             this.btnAdd.TabIndex = 13;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnUpdate
             // 
--- testing2/testing2/WindowsCustomerUI/Form1.cs
+++ testing2/testing2/WindowsCustomerUI/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CustomerLibrary;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,24 @@ namespace WindowsCustomerUI
         public fCustomerScreen()
         {
             InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            CustomerBase custbase = null;
+            if (cbCustomerType.SelectedIndex == 0)
+            {
+                custbase = new Lead();
+            }
+            else
+            {
+                custbase = new Customer();
+            }
+            custbase.CustomerName = tbCustomerName.Text;
+            custbase.Address = rtbAddress.Text;
+            custbase.PhoneNumber = tbPhoneNumber.Text;
+            custbase.BillDate = Convert.ToDateTime(tbBillDate.Text);
+            custbase.BillAmount = Convert.ToDecimal(tbBillAmount.Text);
         }
     }
 }
